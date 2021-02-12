0f49676e3aac946f075bc529cd34a25946b49c6d
davidakoontz <david@koontz.name>
2021-02-12 17:09:31 -0600

    Initial Commit

---
+++ Menu_Example_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Menu_Example_Test
+//
+//  Created by David on 2/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Menu_Example_Test/Menu_Example_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_Example_TestApp.swift
+//  Menu_Example_Test
+//
+//  Created by David on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Menu_Example_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
